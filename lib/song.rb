af27a25323b58bbb8fa1cd45a038df5eeb6ad731
Jacqueline Lee <anginblu@gmail.com>
2017-11-25 05:28:24 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,8 +14,13 @@ class Song
     @name = name
 
     @artist = artist
+    if @@artists.include?("#{@artist}") == false
       @@artists << @artist
       @@artist_count[@artist] += 1
+    else
+      @@artists << @artist
+      @@artist_count[@artist] = @@artist_count[@artist] += 1
+    end
 
     @genre = genre
     if @@genres.include?("#{@genre}") == false
