4e66a2df307e6eeb95df5f307da937f9f50ca820
Jacqueline Lee <anginblu@gmail.com>
2017-11-25 05:03:09 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,6 +10,14 @@ class Song
 
   def initialize(name, artist, genre)
     @name = name
+    @artist = artist
+    artists << @artist
+    artist_count(@artist) =
+    @genre = genre
+    if genres.include? @genre == false
+      genres << @genre
+    end
+    genre_
     @@count += 1
   end
 
